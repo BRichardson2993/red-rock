063fd93b6adf93f67323274934b9936c299532c8
Brian Richardson <Brian Richardson>
2016-06-09 01:57:06 -0500

    corrections to board member listings

--- board.php
+++ board.php
@@ -1,53 +1,51 @@
 <?php include('includes/header.php');?>
-    <main>
-        <div id="wrapper">
-            <section id="col1">
-                <?php include('includes/right-sidebar.php');?>
+  <main>
+    <div id="wrapper">
+      <section id="col1">
+        <?php include('includes/right-sidebar.php');?>
+      </section>
+
+      <section id="col2">
+        <?php include('includes/resp-menu.php');?>
+
+
+          <article class="board">
+            <h1>Oklahoma Mental Health Council</h1>
+              <h2>d/b/a Red Rock Behavioral Health Services</h2>
+              <h2>FY2016</h2>
+                <h3>Board of Directors:</h3>
+
+                <div class="officers">
+                    <ul>
+                        <li>Peggy Cunningham, <span class="position">President</span></li>
+
+                        <li>Dr. John A. Ludrick, <span class="position">Vice President</span></li>
+
+                        <li>Rev. Robert D. Johnson, <span class="position">Secretary</span></li>
+
+                        <li>Mark Wesner, <span class="position">Treasurer</span></li>
+                        <li>Douglas G. Howard, <span class="position">Past President</span></li>
+                    </ul>
+                </div>
+
+                    <ul>
+                        <li>Randy C. Smith</li>
+
+                        <li>Mr. Sherman Menser</li>
+
+                        <li>Mr. Joseph Jackson</li>
+
+                        <li>Mike Mosley</li>
+
+                        <li>James H. Bellingham</li>
+
+                        <li>Robert Heffron</li>
+
+                        <li>Henry Townsend, Ph.D</li>
+                    </ul>
+
+                </article>
             </section>
-
-            <section id="col2">
-                <?php include('includes/resp-menu.php');?>
-
-
-                    <article class="board">
-                       <h1>Oklahoma Mental Health Council<h1>
-                    <h2>d/b/a Red Rock Behavioral Health Services<br>
-                    FY2015</h2>
-                        <h3>Board of Directors:</h3>
-
-                        <div class="officers">
-
-
-                            <ul>
-                                <li>Peggy Cunningham, <span class="position">President</span></li>
-
-                                <li>Dr. John A. Ludrick, <span class="position">Vice President</span></li>
-
-                                <li>Rev. Robert D. Johnson, <span class="position">Secretary</span></li>
-
-                                <li>Mark Wesner, <span class="position">Treasurer</span></li>
-                            </ul>
-                        </div>
-                        <ul>
-                            <li>Randy C. Smith</li>
-
-                            <li>Sherman Menser</li>
-
-                            <li>Joseph Jackson</li>
-
-                            <li>James H. Bellingham</li>
-
-                            <li>Robert Heffron</li>
-
-                            <li>Henry Townsend, Ph.D</li>
-
-                            <li>Mike Mosley</li>
-                        </ul>
-
-                    </article>
-            </section>
-
-
         </div>
     </main>
 
