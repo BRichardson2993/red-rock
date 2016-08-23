42c678251d52793f27e2603343c1425771c2c33a
Brian Richardson <Brian Richardson>
2016-08-22 19:04:06 -0500

    added message from CEO modal

--- index.php
+++ index.php
@@ -32,12 +32,37 @@
             <section class="columns">
 
               <p>Red Rock Behavioral Health Services is a private not&dash;for&dash;profit community mental health center that was founded in 1974, and is based in Oklahoma City. Red Rock provides mental health services, and referrals to physical health services, to indigent and insured Oklahomans, and is one of the largest community mental health centers in Oklahoma.</p>
-              <p>Red Rock has offices in 13 Oklahoma cities and provides services from 27 locations. Across the Company, Red Rock employs nearly 500 full and part&dash;time staff working in Oklahoma City, Chandler, Yukon, El Reno, Chickasha, Watonga, Kingfisher Clinton, Elk City, Hobart, and Altus.</p>
+              <p>Red Rock has offices in 13 Oklahoma cities and provides services from 27 locations. Across the Company, Red Rock employs nearly 500 full and part&dash;time staff working in Oklahoma City, Shawnee, Norman, Chandler, Yukon, El Reno, Chickasha, Watonga, Kingfisher Clinton, Elk City, Hobart, and Altus.</p>
             </section>
             <section class="mission">
               <h2>Mission Statement</h2>
 
               <p>To positively change the lives of youth and adults by providing and coordinating their behavioral and physical health in an effort to strengthen families and support the community.</p>
+            </section>
+            <section class="mission">
+              <h3><i>A Message from the CEO</i></h3>
+              <p>In 1966 a small group of passionate community leaders came together with a vision to form what in 1974 became &ldquo;Red Rock&rdquo;. Red Rock started with a budget of &#36;50,000 and today operates with a &#36;35,000,000 budget... <a href="#openModalCEO" target="_self">more</a>
+
+
+                  <div id="openModalCEO" class="modalDialog">
+                   <div class="ceoMessage">
+                    <a href="#close" title="Close" class="close">X</a>
+                    <p>In 1966 a small group of passionate community leaders came together with a vision to form what in 1974 became “Red Rock”.  Red Rock started with a budget of $50,000 and today operates with a $35,000,000 budget and is one of the largest Community Mental Health Centers (CMHC’s) in Oklahoma serving a total of 25 counties with nearly 500 employees, 16 outpatient offices, 7 residential facilities, an 82 bed Substance Abuse Residential Facility (Jordan’s Crossing) and 4 Crisis Stabilization Units.  In FY 2015, Red Rock served almost 10,000 individuals and families.</p>
+
+                    <p>This past year was challenging and exciting.  Despite budget cuts and a difficult political climate, Red Rock was able to open a beautiful new state of the art, trauma informed, children’s facility, Planet Rock, in Oklahoma City.  This program and building design had been a dream for many years, and we are very excited to see it come to fruition.  Planet Rock is able to offer a full time child psychiatrist who is dedicated to providing services to children, youth and families across Oklahoma. </p>
+
+                    <p>Red Rock transitioned to a new electronic health record (EHR), Netsmart’s my Avatar.  This was a significant financial and time investment but has resulted in Red Rock having a state of the art EHR to meet the clinical and technological challenges of the future.  This past spring, Red Rock continued with our National Accreditation by the Commission on The Accreditation of Rehabilitation Facilities (CARF) and received a 3 year accreditation with the best survey results since Red Rock became accredited in 1992.</p>
+
+                    <p>In an effort to continue to expand our array of services, particularly in substance abuse services, Red Rock received additional certifications in several locations, by the Oklahoma Department of Mental Health and Substance Abuse Services, as a Comprehensive Community Addiction Recovery Center (CCARC).  This allows us to better meet the needs of those we serve with substance abuse issues.  Red Rock also received a planning grant in a competitive bidding process through the Oklahoma Department of Mental Health to work toward meeting standards for national certification as a Certified Community Behavioral Health Clinic.   This national certification process is a result of the Excellence in Mental Health Act, passed by Congress in 2014, in an effort to bring back the federal status of Community Mental Health Centers and consistency to our country for comprehensive, robust mental health services.</p>
+
+                    <p>This past year also resulted in significant growth of our Health Teams enabling us to provide timely, appropriate integrated behavioral health and coordination of primary health care services aimed at treating the whole person.  Statistics show that Oklahoman’s that have a mental illness and/or substance abuse disorder are dying on average 30 years younger than the general population.   The Affordable Care Act, bringing some of the most significant reforms to behavioral health in our nation’s history, continues to result in expanded coverage, parity, and advances in the use of technology to improve access and reduce costs in the future.</p>
+
+                    <p>The years ahead appear full of possibilities, challenges and are filled with rapid change as health care in this country continues to quickly evolve.  Our focus for FY 2017 include continued expansion of our substance abuse services including Medication Assisted Treatment and improving and expanding our services to meet the new healthcare landscape as it continues to move toward value based purchasing and improving quality of health care and lowering costs.</p>
+
+                    <p>-Verna Foust, CEO</p>
+                  </div>
+                  </div>
+
             </section>
 
           </article>
